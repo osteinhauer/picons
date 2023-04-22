e07207c277b5d62069bef707a7dd61f628190b6b
Oliver Steinhauer <oliver.steinhauer@gmail.com>
2023-04-22 12:11:01 +0200

    windows build

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ rm -rf dist
 mkdir dist
 env CGO_ENABLED=0 go build -o dist/picons-update
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o dist/picons-update-arm
+env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/picons-update.exe
 
 chmod 0755 dist/picons-update*
