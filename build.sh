d022ea75ff526a61f68e064bd8c90ec39ab2dabc
Oliver Steinhauer <oliver.steinhauer@gmail.com>
2023-04-23 09:13:58 +0200

    multi arch

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 rm -rf dist
 mkdir dist
-env CGO_ENABLED=0 go build -o dist/picons-update
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/picons-update-amd64-linux
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/picons-update-amd64-darwin
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o dist/picons-update-arm
 env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/picons-update.exe
 
